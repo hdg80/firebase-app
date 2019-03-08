33f7b4708179c016822d7995979b2adfdcb1d847
Heath George <hdgr34gtr@gmail.com>
2019-03-09 02:33:00 +1100

    added so js test just to make sure its working

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+// some junk js just to make sure its working
+
 console.log("I am working");
+
+function addTwoNumbers(a,b){
+    let answer = a + b;
+    return answer;
+}
+
+function SubtractTwoNumbers(a,b){
+    let answer = a - b;
+    return answer;
+}
+
+function DivideTwoNumbers(a,b){
+    let answer = a / b;
+    return answer;
+}
+
+function MultiplyTwoNumbers(a,b){
+    let answer = a * b;
+    return answer;
+}
+
+console.log(addTwoNumbers(2,2));
+console.log(SubtractTwoNumbers(10,2));
+console.log(DivideTwoNumbers(25,5));
+console.log(MultiplyTwoNumbers(5,5));
